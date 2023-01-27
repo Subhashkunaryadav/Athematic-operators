cee374dd153534b54602d79682af34bb06b45545
subhash kumar yadav <110653851+Subhashkunaryadav@users.noreply.github.com>
2023-01-27 09:33:12 +0530

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,57 @@
+x = 10
+y = 2
+z = x + y
+print(z)
+
+x = 30
+y = 10
+a = x + y
+print(a)
+
+x = 10
+y = 10
+z = 10
+u = x + y + z
+print(u)
+
+x = 5
+y = 5
+z = x * x
+print(z)
+
+x = 2
+y = 2
+z = 2
+u = x * y * z
+print(u)
+
+x = 20
+y = 5
+z = x / y
+print(z)
+
+x = 100
+y = 20
+z = x / y
+print(z)
+
+x = 8
+y = 4
+z = x % y
+print(z)
+
+x = 8
+y = 4
+z = x // y
+print(z)
+
+x = float(input('Enter the 1st number'))
+y = float(input('Enter the 2nd number'))
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+print("addition:" , addition)
+print("subtraction:" , subtraction)
+print("multiplication:" , multiplication)
+print("division:" , division)
